e3fbd6fff476fc10da029374f0c9d0ae0c9f3ef4
Robert Lehmann <lehmrob@gmail.com>
2021-02-21 18:05:11 +0100

    WIP: Current state

--- dump.py
+++ dump.py
@@ -2,11 +2,41 @@ import node
 import argparse
 import pickle
 
+def print_dict(data):
+    for index, values in data.items():
+        print(f'    --{hex(index)}')
+        if type(values) == list:
+            for val in values:
+                print(f'      > {val}')
+        else:
+            print(f'      > {values}')
+
 parser = argparse.ArgumentParser(description='Print object')
 parser.add_argument('-f', '--file', action='store', dest='file')
 
 args = parser.parse_args()
 
-with open(args.file, 'rb') as f:
-    node = pickle.load(f)
-    print(f'{node.Dictionary}')
+node = node.Node()
+
+node.load_from_file(args.file)
+
+# with open(args.file, 'rb') as f:
+    # node = pickle.load(f)
+
+print(f'OD File for {node.Name}')
+print(f'  Type:          {node.Type}')
+print(f'  ID:            {node.ID}')
+print(f'  Description:   {node.Description}')
+print(f'  Profile Name:  {node.ProfileName}')
+print(f'  Profile:       {node.Profile}')
+print(f'  Dictionary:')
+
+print_dict(node.Dictionary)
+
+print(f'  ParamsDictionary:')
+
+print_dict(node.ParamsDictionary)
+
+print(f'  Usermappings:')
+
+print_dict(node.UserMapping)
--- node/__init__.py
+++ node/__init__.py
@@ -1,6 +1,9 @@
+import pickle
+
 class Node:
-    def __init__(self, name, type, id, description, profilename, profile, 
-            specificmenu, dictionary, params_dict, ds302, user_mappings):
+    def __init__(self, name = "", type = "", id = 0, description = "", profilename = "", 
+            profile="", specificmenu = None, dictionary = None, params_dict = None, 
+            ds302 = None, user_mappings = None):
         self.Name = name
         self.Type = type
         self.ID = id
@@ -12,3 +15,27 @@ class Node:
         self.ParamsDictionary = params_dict
         self.DS302 = ds302
         self.UserMapping = user_mappings
+
+    def load_from_file(self, filename):
+        with open(filename) as f:
+            data = pickle.load(f)
+
+        self.Name = data.Name
+        self.Type = data.Type
+        self.Type = data.Type
+        self.ID = data.ID
+        self.Description = data.Description
+        self.ProfileName = data.ProfileName
+        self.Profile = data.Profile
+        self.SpecificMenu = data.SpecificMenu
+        self.Dictionary = data.Dictionary
+        self.ParamsDictionary = data.ParamsDictionary
+        self.DS302 = data.DS302
+        self.UserMapping = data.UserMapping
+
+
+class ObjectEntry:
+    def __init__(self, name:str, od_type:int, access:str):
+        self.name = name
+        self.od_type = od_type,
+        self.access = access
